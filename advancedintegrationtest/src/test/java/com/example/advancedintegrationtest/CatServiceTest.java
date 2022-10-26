591f258fe6b24f60f7bd4a583f0536dfc2d8fa62
Sluchyk V.V <boss.sluchyk@gmail.com>
2022-10-26 23:59:01 +0300

    fixedTest

--- advancedintegrationtest/src/test/java/com/example/advancedintegrationtest/CatServiceTest.java
+++ advancedintegrationtest/src/test/java/com/example/advancedintegrationtest/CatServiceTest.java
@@ -5,7 +5,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.jsonResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,16 +14,18 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @WireMockTest(httpPort = 8080)
-@TestPropertySource(properties = "cat.url=https://catfact.ninja/fact")
+@TestPropertySource(properties = "cat.url=http://localhost:8080")
 public class CatServiceTest {
     @Autowired
     private CatService catService;
 
     @Test
     void testCatService(WireMockRuntimeInfo runtimeInfo) {
-        Cat cat;
-        stubFor(get("").willReturn(jsonResponse("", HttpStatus.OK.value())));
-        cat = catService.getFact();
-        assertNotNull(cat);
+        Cat cat=new Cat();
+        cat.setFact("cat");
+        cat.setLength(4);
+        stubFor(get("/").willReturn(jsonResponse(cat, HttpStatus.OK.value())));
+        Cat cat1 = catService.getFact();
+        assertEquals(cat,cat1);
     }
 }
